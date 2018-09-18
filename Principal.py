189dd19080f42e89cb7218c5abc256bf24056942
daniel garcia alonso <dg522730@gmail.com>
2018-09-18 17:15:21 -0500

    uso de memoria dinamica

--- Principal.py
+++ Principal.py
@@ -3,9 +3,9 @@ __author__ = 'Daniel Garcia'
 from memoria_estatica import *
 
 
-Panaderia = ['Conchas','Chilindrina','Cuernitos']
+Panaderia = ['Conchas','Chilindrina','Cuernitos','Pan beso','Rosquetes','Regañadas']
 Precios = ['$5','$7','$10']
-MateriaPrima = ['Harina','Agua','Levadura']
+MateriaPrima = ['Harina','Agua','Levadura','manteca','Aceite','Azucar','Huevo','Mermelada']
 
 listas = memDinamica(Panaderia)
 listas.imprimirLista()
@@ -13,9 +13,13 @@ listas.agregarelementoarray('muffin')
 listas.imprimirLista()
 listas.agregarelementoarray('Pan blanco')
 listas.imprimirLista()
-listas.ordenarLista()
-listas.imprimirLista()
 listas.eliminarElemento('Chilindrina')
+listas.imprimirLista()
+listas.insertarNPosicion(6,'Trocante')
+listas.imprimirLista()
+listas.eliminarNElementos(0)
+listas.imprimirLista()
+listas.ordenarLista()
 listas.imprimirLista()
 
 
@@ -23,6 +27,11 @@ listas1 = memDinamica(Precios)
 listas1.imprimirLista()
 listas1.agregarelementoarray('$12')
 listas1.imprimirLista()
+listas1.eliminarNElementos(0)
+listas1.imprimirLista()
+listas1.agregarelementoarray(25)
+listas1.imprimirLista()
+
 
 listas2 = memDinamica(MateriaPrima)
 listas2.imprimirLista()
@@ -30,5 +39,5 @@ listas2.agregarelementoarray('Mano de Obra')
 listas2.imprimirLista()
 listas2.ordenarLista()
 listas2.imprimirLista()
-listas2.insertarNPosicion(1,'acido')
+listas2.insertarNPosicion(1,'Trabajador')
 listas2.imprimirLista()
